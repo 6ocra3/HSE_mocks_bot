3a7d8634076294693b71eaefe77e6c0025e4b783
6ocra3 <6o3kmm@mail.ru>
2023-10-24 12:49:02 +0300

    init commit

---
+++ utils.py
@@ -0,0 +1,30 @@
+import random
+import json
+class Number:
+    def __init__(self, field, min, max):
+        self.field = field
+        self.min = min
+        self.max = max
+
+    def generate(self):
+        return random.randint(self.min, self.max)
+
+class Name:
+    def __init__(self, field):
+        self.field = field
+        self.data = ['Александр', 'Алексей', 'Андрей', 'Анна', 'Артём', 'Дарья', 'Дмитрий', 'Евгения', 'Екатерина', 'Иван', 'Ксения', 'Мария', 'Максим', 'Наталья', 'Никита', 'Ольга', 'Павел', 'Светлана', 'Сергей', 'Татьяна', 'Юлия', 'Яна', 'Алина', 'Анатолий', 'Валерия', 'Виктор', 'Галина', 'Георгий', 'Елена', 'Илья', 'Кирилл', 'Лариса', 'Любовь', 'Михаил', 'Надежда', 'Олег', 'Полина', 'Роман', 'София', 'Станислав', 'Тимофей', 'Ульяна', 'Федор', 'Христина', 'Эдуард', 'Юрий', 'Ярослав']
+    def generate(self):
+        return random.choice(self.data)
+
+
+mask = [Number("age", 5, 20), Name("name")]
+itog = []
+for i in range(5):
+    a = {}
+    for j in mask:
+        a[j.field] = j.generate()
+    itog.append(a)
+
+print(json.dumps(itog, ensure_ascii=False))
+
+#Name, City, Date, Number, Uuid, Email,PhoneNumber, Login, Password.
